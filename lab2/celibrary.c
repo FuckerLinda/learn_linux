e6cd8ce58629c032943fbda312b931fda3e22c1f
FuckerLinda <73421721+FuckerLinda@users.noreply.github.com>
2025-03-11 20:24:29 +0800

    sds

---
+++ lab2/celibrary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <endian.h>
+
+void check_endian_standard() {
+    if (__BYTE_ORDER == __LITTLE_ENDIAN) {
+        printf("Little Endian (Standard Library Method)\n");
+    } else if (__BYTE_ORDER == __BIG_ENDIAN) {
+        printf("Big Endian (Standard Library Method)\n");
+    }
+}
+
+int main() {
+    check_endian_standard();
+    return 0;
+}
