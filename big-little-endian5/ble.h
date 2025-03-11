24e80a9b26baef882c52d2cfe801cec68f705941
FuckerLinda <73421721+FuckerLinda@users.noreply.github.com>
2025-03-11 21:20:51 +0800

    6.  新建目录，按下面目录拷贝要求3中代码，编写 Makefile 编译运行代码，给出运行结果。使用 git 记录过程（4分）
    6.
    
    新建目录，按下面目录拷贝要求3中代码，编写 Makefile 编译运行代码，给出运行结果。使用 git 记录过程（4分）

---
+++ big-little-endian5/ble.h
@@ -0,0 +1,10 @@
+#ifndef BLE_H
+#define BLE_H
+
+void check_endian_bit_field();
+void check_endian_bitwise();
+void check_endian_standard();
+void check_endian_pointer();
+void check_endian_union();
+
+#endif
---
+++ big-little-endian5/src/ceunion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void check_endian_union() {
+    union {
+        unsigned int i;
+        unsigned char c[4];
+    } test_union;
+
+    test_union.i = 0x12345678;
+
+    if (test_union.c[0] == 0x78) {
+        printf("Little Endian (Union Method)\n");
+    } else {
+        printf("Big Endian (Union Method)\n");
+    }
+}
+
