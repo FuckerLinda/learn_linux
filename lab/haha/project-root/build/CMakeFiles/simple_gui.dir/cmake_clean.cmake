6e66442cbe5329080e180fa5a1aa78c6b2f3d5dc
FuckerLinda <73421721+FuckerLinda@users.noreply.github.com>
2025-04-04 09:46:40 +0800

    项目地基准备完毕

---
+++ lab/haha/project-root/build/CMakeFiles/simple_gui.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_gui_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/simple_gui_autogen.dir/ParseCache.txt"
+  "simple_gui_autogen"
+  "CMakeFiles/simple_gui.dir/gui/mainwindow.cpp.o"
+  "CMakeFiles/simple_gui.dir/gui/mainwindow.cpp.o.d"
+  "CMakeFiles/simple_gui.dir/simple_gui_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/simple_gui.dir/simple_gui_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/simple_gui.dir/src/main.cpp.o"
+  "CMakeFiles/simple_gui.dir/src/main.cpp.o.d"
+  "simple_gui"
+  "simple_gui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- lab/hello.c
+++ lab/hello.c
@@ -3,6 +3,10 @@
 
 int main() {
     double result = sqrt(2.0);
+    int a=3;
     printf("Square root: %f\n", result);
+    for (;a<10;a++){
+	    printf("a=%d",a);
+    }
     return 0;
 }
